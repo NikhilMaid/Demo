19d473bc050083d0a73ce7d75f43247cc8ff869f
Nikhil Maid <nikhilmaid318@gmail.com>
2024-07-01 12:29:37 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -12,46 +12,46 @@ import Categories from "./Pages/Categories";
 import Cart from "./Pages/Cart";
 import Shop from "./Pages/Shop";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "",
-//         element: <Home />,
-//       },
-//       {
-//         path: "About",
-//         element: <About />,
-//       },
-//       {
-//         path: "Categories",
-//         element: <Categories />,
-//       },
-//       {
-//         path: "Cart",
-//         element: <Cart />,
-//       },
-//       {
-//         path: "Shop",
-//         element: <Shop />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "About",
+        element: <About />,
+      },
+      {
+        path: "Categories",
+        element: <Categories />,
+      },
+      {
+        path: "Cart",
+        element: <Cart />,
+      },
+      {
+        path: "Shop",
+        element: <Shop />,
+      },
+    ],
+  },
+]);
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="About" element={<About />} />
-      <Route path="Shop" element={<Shop />} />
-      <Route path="Categories" element={<Categories />} />
-      <Route path="Cart" element={<Cart />} />
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route path="" element={<Home />} />
+//       <Route path="About" element={<About />} />
+//       <Route path="Shop" element={<Shop />} />
+//       <Route path="Categories" element={<Categories />} />
+//       <Route path="Cart" element={<Cart />} />
+//     </Route>
+//   )
+// );
 
 const App = () => {
   return <RouterProvider router={router} />;
