316c2aa5f1f6ab80cc0c7120369b21701b7fb69f
Nikhil Maid <nikhilmaid318@gmail.com>
2024-07-01 12:16:16 +0530

    myav

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./globals.css";
+import Home from "./Pages/Home";
+import Layout from "./Layout";
+import About from "./Pages/About";
+import Categories from "./Pages/Categories";
+import Cart from "./Pages/Cart";
+import Shop from "./Pages/Shop";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "About",
+//         element: <About />,
+//       },
+//       {
+//         path: "Categories",
+//         element: <Categories />,
+//       },
+//       {
+//         path: "Cart",
+//         element: <Cart />,
+//       },
+//       {
+//         path: "Shop",
+//         element: <Shop />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="About" element={<About />} />
+      <Route path="Shop" element={<Shop />} />
+      <Route path="Categories" element={<Categories />} />
+      <Route path="Cart" element={<Cart />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <div className="underline font-bold text-6xl">Hello world!</div>
+    </>
+  );
+};
+
+export default Home;
